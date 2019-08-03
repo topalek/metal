83d9a1e4f2091c247cc382f57ee82b6c05cf6047
topalek <topalek@mail.ru>
2019-08-03 12:43:17 +0300

    правка "отложить в деловой"

--- core/views/site/about.php
+++ core/views/site/about.php
@@ -11,17 +11,16 @@ $date     = '2019-04-02 00:00:00';
 $date     = date('Y-m-d 00:00:00');
 $fromDate = date('Y-m-d 00:00:00', strtotime($date . "-1 day"));
 $toDate   = date('Y-m-d 00:00:00', strtotime($date . "+1 day"));
-
+$id       = 1;
+$model    = Product::findOne($id);
 
 $headers = Product::getCachePrice();
 ?>
 <div class="site-about" style="margin-top: 60px">
-    <div class="col-md-12">
-        <pre>
-<!--            --><?php //print_r(Product::getEmptyArray()); ?>
-        </pre>
+    <pre>
+        <?php print_r($model); ?>
+    </pre>
 
-    </div>
     <pre>
 <?php print_r($headers) ?>
     </pre>
